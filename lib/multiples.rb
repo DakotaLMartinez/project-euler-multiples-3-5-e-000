d53e7ef8319744b5908cff8b5ce4007a5adc8c37
DakotaLMartinez <dakotaleemusic@gmail.com>
2015-11-10 06:28:26 +0000

    Adds collect_multiples function designed to return an array of the multiples of 3 and 5 between 1 and the argument passed to the function

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
-# Enter your procedural solution here!
+sum = 0
+def collect_multiples(limit)
+  multiples = Array.new
+  counter = 1
+  while counter < limit.to_i
+    if counter % 3 == 0 || counter % 5 == 0
+      multiples.push(counter)
+    end
+    counter += 1
+  end
+  multiples
+end
