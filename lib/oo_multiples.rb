dce9b40a6c85b6752858090c03bba9dca8083ae4
DakotaLMartinez <dakotaleemusic@gmail.com>
2015-11-10 06:49:58 +0000

    adds object oriented version of the function that returns the sum of all multiples of 3 and 5

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
-# Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    multiples = Array.new
+    counter = 1
+    while counter < @limit.to_i
+      if counter % 3 == 0 || counter % 5 == 0
+        multiples.push(counter)
+      end
+      counter += 1
+    end
+    multiples
+  end
+
+  def sum_multiples
+    sum = 0
+    collect_multiples.each do |multiple|
+      sum += multiple
+    end
+    sum
+  end
+end
